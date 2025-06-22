5fc052966b518a3a017dc9b7d524ea9657d82a5e
dangdang1ing15 <shine_0512@naver.com>
2025-06-22 12:56:28 +0900

    [Setting] Add Projects

---
+++ AnglesRecord_IOS/AnglesRecord_IOS/AnglesRecord_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnglesRecord_IOSApp.swift
+//  AnglesRecord_IOS
+//
+//  Created by 성현 on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AnglesRecord_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnglesRecord_IOS/AnglesRecord_IOSTests/AnglesRecord_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnglesRecord_IOSTests.swift
+//  AnglesRecord_IOSTests
+//
+//  Created by 성현 on 6/22/25.
+//
+
+import Testing
+@testable import AnglesRecord_IOS
+
+struct AnglesRecord_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
