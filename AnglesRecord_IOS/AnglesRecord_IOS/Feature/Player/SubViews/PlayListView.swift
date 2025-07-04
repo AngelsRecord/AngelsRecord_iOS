0897a91a8cc72e1b4e5a64f2e48a369e4dff54e4
hiseyeon <se020122@naver.com>
2025-07-04 17:12:24 +0900

    [Feat]#26 - PlayListView 파일 생성

---
+++ AnglesRecord_IOS/AnglesRecord_IOS/Feature/Player/SubViews/PlayListView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayListView.swift
+//  AnglesRecord_IOS
+//
+//  Created by Hwnag Seyeon on 7/4/25.
+//
+
+import SwiftUI
+
+struct PlayListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayListView()
+}
