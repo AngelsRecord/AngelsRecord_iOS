65657c98749df3cb908e666c7d2934d783afefb9
sep037 <tmddms0307@dankook.ac.kr>
2025-08-12 19:43:01 +0900

    Feat: 토스트메세지 추가 #74

--- AnglesRecord_IOS/AnglesRecord_IOS/Components/ToastMessage.swift
+++ AnglesRecord_IOS/AnglesRecord_IOS/Components/ToastMessage.swift
@@ -6,35 +6,37 @@
 //
 import SwiftUI
 
-struct EKOToastMessage: View {
+struct ToastMessage: View {
     
     var body: some View {
         HStack(spacing: 8) {
                 Circle()
                     .frame(width: 15, height: 15)
-                    .foregroundStyle(.mainWhite)
+                    .foregroundStyle(.toastBlack)
                     .overlay {
                         Image(systemName: "exclamationmark.circle.fill")
                             .resizable()
                             .scaledToFit()
-                            .foregroundStyle()
+                            .foregroundStyle(.toastRed)
                             .frame(width: 12, height: 12)
                     }
 
-            Text("인증코드를 확인해 주세요.")
-                .font(.textSemiBold02)
-                .foregroundColor(.neutrals2)
+            Text("인증코드를 확인해 주세요")
+                .font(.system(size:14, weight: .semibold))
+                .foregroundColor(.buttonText)
         }
         .padding(.horizontal, 16)
         .padding(.vertical, 10)
-        .setupGradientBackground(colors: [
-            .mainWhite.opacity(0.8), .mainWhite.opacity(0.5)
-        ])
-        .cornerRadius(28)
-        .overlay(
-            RoundedRectangle(cornerRadius: 28)
-                .stroke(Color.white.opacity(0.3), lineWidth: 0.5)
-        )
-        .shadow(color: .black.opacity(0.05), radius: 1, x: 0, y: 1)
+        .background(.toastBlack)
+        .cornerRadius(20)
+//        .overlay(
+//            RoundedRectangle(cornerRadius: 28)
+//                .stroke(Color.white.opacity(0.3), lineWidth: 0.5)
+//        )
+//        .shadow(color: .black.opacity(0.05), radius: 1, x: 0, y: 1)
     }
+}
+
+#Preview {
+    ToastMessage()
 }
