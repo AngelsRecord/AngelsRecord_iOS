eda7e7ecf4a8098530b3f9aa31d94a8a2403c619
dangdang1ing15 <shine_0512@naver.com>
2025-08-12 12:27:57 +0900

    [Chore] 테스트코드 제거

--- AnglesRecord_IOS/AnglesRecord_IOS/Application/AppDelegate.swift
+++ AnglesRecord_IOS/AnglesRecord_IOS/Application/AppDelegate.swift
@@ -31,8 +31,8 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         // ✅ Firebase 초기화
         FirebaseApp.configure()
         
-        KeychainHelper.delete("verifiedAccessCode")
-        print("🧹 테스트용 Keychain 삭제 완료")
+//        KeychainHelper.delete("verifiedAccessCode")
+//        print("🧹 테스트용 Keychain 삭제 완료")
 
         // ✅ 오디오 세션
         do {
