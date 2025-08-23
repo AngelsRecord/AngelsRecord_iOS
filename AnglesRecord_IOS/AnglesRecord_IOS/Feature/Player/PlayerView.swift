51ac383e51efbbd4db74867d8d760ee47f8f5037
dangdang1ing15 <shine_0512@naver.com>
2025-08-24 03:53:33 +0900

    [Chore] 불필요 코드 제거

--- AnglesRecord_IOS/AnglesRecord_IOS/Feature/Player/PlayerView.swift
+++ AnglesRecord_IOS/AnglesRecord_IOS/Feature/Player/PlayerView.swift
@@ -445,3 +445,13 @@ class SystemVolumeObserver {
         observation?.invalidate()
     }
 }
+
+struct HiddenSystemVolumeView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MPVolumeView {
+        let view = MPVolumeView(frame: .zero)
+        view.alpha = 0.0001
+        return view
+    }
+
+    func updateUIView(_ uiView: MPVolumeView, context: Context) {}
+}
